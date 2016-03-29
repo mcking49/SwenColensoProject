8b849c0113b76faddce3233727935e5963e2a8f7
Miten Chauhan <mitenchauhan@hotmail.com>
2016-03-30 12:56:06 +1300

    Explore functionality Added purpose to the Explore page so now it displays the whole Colenso database and allows users to click on documents to view. Currently documents are not being rendered on a new page.

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Colenso Project' });
 });
 
-router.get("/explore",function(req,res) {
-    res.render('explore', {title: 'Colenso Project '});
+router.get("/explore",function(req,res){
+    client.execute("XQUERY db:list('Colenso')",
+        function (error, result) {
+            if(error){ console.error(error);}
+            else {
+                var splitlist = result.result.split("\n")
+                res.render('explore', { title: 'Colenso Project', fileNames: splitlist });
+            }
+        }
+    );
 });
+
+
+
+
+
+
 module.exports = router;
