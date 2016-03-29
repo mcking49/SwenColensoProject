32881c0c8da4c26d015c80f5609e17a17bb65eb4
Miten Chauhan <mitenchauhan@hotmail.com>
2016-03-30 12:51:36 +1300

    BaseX Connection Added BaseX connection to the website so it now works with the basex database server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+client.execute("OPEN Colenso");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Colenso Project' });
 });
+
 
 module.exports = router;
