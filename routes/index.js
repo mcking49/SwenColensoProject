45e9a105cf00e9ebd4c77bceac43423a4bfa96dd
Miten Chauhan <mitenchauhan@hotmail.com>
2016-03-31 00:47:16 +1300

    Raw Txt and Tidy Up re did the raw txt page and allows user to switch back to readable format and download the file. tidied up the code.

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,13 @@ var router = express.Router();
 var basex = require('basex');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 client.execute("OPEN Colenso");
-var rawToReadable = "";
-var rawQuery = "";
-var loop = false;
 
-// global variable for downloading a file.
 var downloadFilename = "";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Colenso Project' });
 });
-
-/*router.get('/download', function(req, res) {
-    res.render('download', { title: 'Download File' });
-});*/
 
 router.get("/explore",function(req,res){
     client.execute("XQUERY db:list('Colenso')",
@@ -31,75 +23,41 @@ router.get("/explore",function(req,res){
     );
 });
 
-router.get("/rawToFile",function(req,res){
-    client.execute(rawToReadable,
+router.get("/file",function(req,res){
+    downloadFilename = req.query.filename;
+    var query = "XQUERY doc('Colenso/" + downloadFilename + "')";
+    client.execute(query,
         function (error, result) {
-            if(error){ console.error(error);}
+            if (error) {
+                console.error(error);
+            }
             else {
-                var splitlist = result.result.split("\n")
-                downloadFilename = req.query.filename;
-                res.render('file', { title: 'Colenso Project', data: splitlist });
+                var name = result.result.match(/<title>[\s\S]*?<\/title>/)[0];
+                name = name.replace("<title>", "");
+                name = name.replace("</title>", "");
+                res.render('file', {title: 'Colenso Project', downloadFilename: name, data: result.result});
             }
         }
     );
 });
 
-router.get("/file",function(req,res){
-    if(loop === true){
-        client.execute(rawToReadable,
-            function (error, result) {
-                if(error){ console.error(error);}
-                else {
-                    var splitlist = result.result.split("\n")
-                    downloadFilename = req.query.filename;
-                    loop = true;
-                    res.render('file', { title: 'Colenso Project', data: splitlist });
-                }
-            }
-        );
-    }else {
-        var query = "XQUERY doc('Colenso/" + req.query.filename + "')";
-        rawToReadable = query;
-
-        client.execute(query,
-            function (error, result) {
-                if (error) {
-                    console.error(error);
-                }
-                else {
-                    var splitlist = result.result.split("\n")
-                    downloadFilename = req.query.filename;
-                    loop = true;
-                    res.render('file', {title: 'Colenso Project', data: splitlist});
-                }
-            }
-        );
-    }
-});
-
 router.get("/rawFile",function(req,res){
-    rawQuery = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-        "(doc('Colenso/" + downloadFilename + "'))[1]";
-    client.execute(rawQuery,
+    client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+        "(doc('Colenso/" + downloadFilename + "'))[1]",
         function (error, result) {
             if(error){ console.error(error);}
             else {
-                var splitlist = result.result.split("\n")
-                downloadFilename = req.query.filename;
-                res.render('rawFile', { title: 'Colenso Project', data: splitlist });
+                res.render('rawFile', { title: 'Colenso Project', data: result.result });
             }
         }
     );
 });
 
 router.get('/database', function(req, res) {
-
     var tei = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';";
     if(req.query.databaseSearch){
         var searchArray = req.query.databaseSearch.split(" ");
         var queryCondition = "";
-
-
         if(1 < searchArray.length){
             if(searchArray[0] === "NOT") {
                 queryCondition += "' ftand ftnot '";
@@ -115,7 +73,6 @@ router.get('/database', function(req, res) {
                 } else if (searchArray[1] === "NOT") {
                     queryCondition += "' ftand ftnot '";
                     queryCondition += searchArray[2];
-
                 } else {
                     queryCondition += " ";
                     queryCondition += searchArray[1];
@@ -125,22 +82,16 @@ router.get('/database', function(req, res) {
             queryCondition += searchArray[0];
         }
     }
-    console.log(queryCondition);
-
-     var searchQuery = tei + "for $t in (collection('Colenso')[. contains text ' "  + queryCondition +"' using wildcards])\n" +
-        "return concat('<a href=\"/file?filename=', db:path($t), '\" class=\"searchResult\">', '</a>'," +
-        "'<p class=\"searchResult\">', db:path($t), '</p>')";
-
-    client.execute(searchQuery,
+    client.execute(tei + "for $t in (collection('Colenso')[. contains text ' "  + queryCondition +"' using wildcards])\n" +
+        "return db:path($t)",
         function (error, result) {
             if(error){ console.error(error)}
             else {
                 if(req.query.databaseSearch == undefined || req.query.databaseSearch == null){
                     res.render('database', { title: 'Colenso Project', results: " "});
                 }else{
-                   var nResults = (result.result.match(/<\/a>/g) || []).length;
-                    var splitlist = result.result.split("\n")
-                    res.render('database', { title: 'Colenso Project', results: splitlist , nResults : nResults});
+                    var splitlist = result.result.split("\n");
+                    res.render('database', { title: 'Colenso Project', results: splitlist});
                 }
             }
         }
@@ -158,12 +109,11 @@ router.get('/download', function(req, res) {
                 res.writeHead(200, {
                     'Content-Type': 'application/force-download','Content-disposition': 'attachment; filename=' + downloadFilename,
                 });
-
                 res.write(result.result);
                 res.end();
-
             }
-        });
+        }
+    );
 });
 
 module.exports = router;
